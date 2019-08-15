48795110ef71760bb5a4acf4a5879a4c02fa8335
unknown <cvlopes88@gmail.com>
2019-08-14 20:26:26 -0400

    2nd

--- src/conponents/DayP.js
+++ src/conponents/DayP.js
@@ -4,9 +4,9 @@ import React from "react";
 
 const DayP = props => {
     return (
-        <div className="day-photo" key={props.id}>
+        <div className="day-photo" key={props.date}>
             <h2>Photo Of The Day: {props.title}</h2>
-            {/* <img className="dayImg" alt="pic of the day" src={props.url}/> */}
+            <img className="dayImg" alt="pic of the day" src={props.photo}/>
             <p>{props.description}</p>
         </div>
     )
